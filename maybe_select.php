d469086ca0f4107b153d786f9df28f5912b98306
Stephen Paul Weber <singpolyma@singpolyma.net>
2009-07-26 19:05:44 -0700

    Append features for maybe_select

--- maybe_select.php
+++ maybe_select.php
@@ -5,7 +5,7 @@ $fselector => what to select (ie: array('parent','child'))
 $to => array you want data in
 $tselector => where to put data
 */
-function maybe_select($from, $fselector, &$to, $tselector) {
+function maybe_select($from, $fselector, &$to, $tselector, $append=false) {
 	if(!is_array($from)) return NULL;
 	$a = $from;
 	foreach((array)$fselector as $sel) {
@@ -19,7 +19,13 @@ function maybe_select($from, $fselector, &$to, $tselector) {
 		if(!$b[$sel]) $b[$sel] = array();
 		$b = &$b[$sel];
 	}
-	$b[$lastsel] = $a;
+	if($append && $b[$lastsel]) {
+		$b[$lastsel] = array_merge((array)$a, (array)$b[$lastsel]);
+	} else if ($append == 'force') {
+		$b[$lastsel] = (array)$a;
+	} else {
+		$b[$lastsel] = $a;
+	}
 }
 
 ?>
